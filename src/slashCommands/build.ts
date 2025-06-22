4676043eaf9b80c382ee235e7f8c4fe13b1e28ff
matheo-prd <perod.matheo@gmail.com>
2025-06-22 22:17:24 +0200

    refactor(build) : amélioration de la lisibilité du code en reformattant les switch et en mettant à jour l'URL par défaut de l'infographie
    - Reformattage des instructions `switch` pour une meilleure clarté et maintenabilité (gestion des armes et des éléments).
    - Mise à jour de l'URL par défaut de l’infographie selon la nouvelle structure du dépôt.
    - Réorganisation de la logique du collecteur de composants de message pour plus de cohérence.

--- src/slashCommands/build.ts
+++ src/slashCommands/build.ts
@@ -54,24 +54,66 @@ export const command: SlashCommand = {
 
             // Gestion de l'arme 
             switch (characterInfos.weapon) {
-                case "Sword": weaponTranslation = "Épée"; weaponEmote = "<:icon_sword:1328118474588815401>"; break;
-                case "Claymore": weaponTranslation = "Claymore"; weaponEmote = "<:icon_claymore:1328118750733537302>"; break;
-                case "Polearm": weaponTranslation = "Lance"; weaponEmote = "<:icon_lance:1328117598683926558>"; break;
-                case "Catalyst": weaponTranslation = "Catalyseur"; weaponEmote = "<:icon_catalyseur:1328118645586526239>"; break;
-                case "Bow": weaponTranslation = "Arc"; weaponEmote = "<:icon_arc:1328116772443787315>"; break;
-                default: weaponTranslation = "Non trouvé"; weaponEmote = ""; break;
+                case "Sword":
+                    weaponTranslation = "Épée";
+                    weaponEmote = "<:icon_sword:1328118474588815401>";
+                    break;
+                case "Claymore":
+                    weaponTranslation = "Claymore";
+                    weaponEmote = "<:icon_claymore:1328118750733537302>";
+                    break;
+                case "Polearm":
+                    weaponTranslation = "Lance";
+                    weaponEmote = "<:icon_lance:1328117598683926558>";
+                    break;
+                case "Catalyst":
+                    weaponTranslation = "Catalyseur";
+                    weaponEmote = "<:icon_catalyseur:1328118645586526239>";
+                    break;
+                case "Bow":
+                    weaponTranslation = "Arc";
+                    weaponEmote = "<:icon_arc:1328116772443787315>";
+                    break;
+                default:
+                    weaponTranslation = "Non trouvé";
+                    weaponEmote = "";
+                    break;
             }
 
             // Couleur de l'embed en fonction de sa vision  
             switch (characterInfos.element) {
-                case "Pyro": embedColor = "#f51e0f"; elementEmote = "<:Pyro:1328111225954893864>"; break;
-                case "Hydro": embedColor = "#0f8df5"; elementEmote = "<:Hydro:1328111284721422418>"; break;
-                case "Anemo": embedColor = "#0ff5a4"; elementEmote = "<:Anemo:1328112279585292298>"; break;
-                case "Cryo": embedColor = "#0fdaf5"; elementEmote = "<:Cryo:1328111431534772284>"; break;
-                case "Electro": embedColor = "#8d0ff5"; elementEmote = "<:Electro:1328111540888408196>"; break;
-                case "Geo": embedColor = "#f5bb0f"; elementEmote = "<:Geo:1328111327020978337>"; break;
-                case "Dendro": embedColor = "#13f50f"; elementEmote = "<:Dendro:1328111354866831440>"; break;
-                default: embedColor = "#ffffff"; elementEmote = ""; break;
+                case "Pyro":
+                    embedColor = "#f51e0f";
+                    elementEmote = "<:Pyro:1328111225954893864>";
+                    break;
+                case "Hydro":
+                    embedColor = "#0f8df5";
+                    elementEmote = "<:Hydro:1328111284721422418>";
+                    break;
+                case "Anemo":
+                    embedColor = "#0ff5a4";
+                    elementEmote = "<:Anemo:1328112279585292298>";
+                    break;
+                case "Cryo":
+                    embedColor = "#0fdaf5";
+                    elementEmote = "<:Cryo:1328111431534772284>";
+                    break;
+                case "Electro":
+                    embedColor = "#8d0ff5";
+                    elementEmote = "<:Electro:1328111540888408196>";
+                    break;
+                case "Geo":
+                    embedColor = "#f5bb0f";
+                    elementEmote = "<:Geo:1328111327020978337>";
+                    break;
+                case "Dendro":
+                    embedColor = "#13f50f";
+                    elementEmote = "<:Dendro:1328111354866831440>";
+                    break;
+                default:
+                    embedColor = "#ffffff";
+                    elementEmote = "";
+                    break;
             }
 
             // Préparation de l'embed
@@ -103,7 +145,7 @@ export const command: SlashCommand = {
                 )
                 .setThumbnail(characterInfos.portraitLink)
                 // Affichage de la première infographie
-                .setImage(characterBuilds[0].url ?? "https://furina.antredesloutres.fr/infographie/default_Snezhnaya.png")
+                .setImage(characterBuilds[0]?.url ?? "https://raw.githubusercontent.com/matheo-1712/Furina/refs/heads/main/api/img/infographies/default_Snezhnaya.png")
                 .setFooter({
                     text: "Crédits : Keqing Mains - Powered by CitlAPI",
                 }).setTimestamp();
@@ -134,9 +176,12 @@ export const command: SlashCommand = {
             }
 
             // Envoi de la réponse avec plusieurs lignes si nécessaire
-            const message = await interaction.reply({ embeds: [embed], components: actionRows });
+            const message = await interaction.reply({embeds: [embed], components: actionRows});
 
-            const collector = message.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60_000 });
+            const collector = message.createMessageComponentCollector({
+                componentType: ComponentType.Button,
+                time: 60_000
+            });
 
             // Edition du message lorsque le bouton est cliqué
             collector.on('collect', async i => {
@@ -150,12 +195,15 @@ export const command: SlashCommand = {
                 // Récupérer l'infographie correspondante
                 const build = characterBuilds.find(build => build.build === button);
                 if (!build) {
-                    await interaction.editReply({ content: 'Une erreur est survenue lors de la récupération de l\'infographie.', components: [] });
+                    await interaction.editReply({
+                        content: 'Une erreur est survenue lors de la récupération de l\'infographie.',
+                        components: []
+                    });
                     return;
                 }
 
                 // Récupération de l'URL de l'infographie
-                const url = build.url;
+                const url = build.url ?? "https://furina.antredesloutres.fr/infographie/default_Snezhnaya.png";
 
                 // Éditer le message initial avec l'embed correspondant
                 await interaction.editReply({
@@ -166,7 +214,7 @@ export const command: SlashCommand = {
             });
 
             collector.on('end', async i => {
-                await interaction.editReply({ components: [] });
+                await interaction.editReply({components: []});
             });
 
         } catch (error) {
